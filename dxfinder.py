13a979967dc6370f9d6c38d2aef5e278482a697e
kvit <vk@pinnacleservice.co>
2021-04-04 21:53:31 -0500

    dowload model from google storage

--- dxfinder.py
+++ dxfinder.py
@@ -3,9 +3,28 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import simple_elmo
+from google.cloud import storage
+import os
 
 st.title('Dx Finder')
 st.write("Find Dx codes for Pathology report")
+
+model_fle="model/model.hdf5"
+st.cache()
+def DownloadModel () :
+    if not os.path.exists(model_fle) :
+        print('Downloading model from cloud storage, please wait..')
+
+        # get file from google storage
+        bucket = 'pp-ligolab-models'
+        source="model_elmo_2/model.hdf5"
+
+        client = storage.Client.create_anonymous_client()
+        bucket = client.bucket(bucket_name=bucket, user_project=None)
+        blob = storage.Blob(source, bucket)
+        blob.download_to_filename(filename=model_fle, client=client)
+
+#------------
 
 elmo = simple_elmo.ElmoModel()
 elmo.load("model")
